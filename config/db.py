8917c923bd54f8eaea79d9cf7101296b130fc9f1
Luis David Betances M <david_bm42@hotmail.com>
2022-08-20 18:35:05 -0400

    right database url

--- config/db.py
+++ config/db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, MetaData
 # DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/serversiderendering"
 
 # engine = create_engine("mysql+pymysql://root@localhost:3306/modernkitchendb")
-CLEARDB_DATABASE_URL = "mysql+pymysql://bd213d79e5d0f0:e9dda132@us-cdbr-east-06.cleardb.net/heroku_cc73e3a97a02270"
+CLEARDB_DATABASE_URL = "mysql+pymysql://bd213d79e5d0f0:e9dda132@us-cdbr-east-06.cleardb.net:3306/heroku_cc73e3a97a02270"
 engine = create_engine(CLEARDB_DATABASE_URL)
 # meta = MetaData()
 
