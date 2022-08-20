3fe8634131a9f7ac0b59942c6219fd74011c8d6b
Luis David Betances M <david_bm42@hotmail.com>
2022-08-20 17:20:09 -0400

    database connection

--- config/db.py
+++ config/db.py
@@ -3,7 +3,8 @@ from sqlalchemy import create_engine, MetaData
 # DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/serversiderendering"
 
 # engine = create_engine("mysql+pymysql://root@localhost:3306/modernkitchendb")
-engine = create_engine("mysql+pymysql://id19445098_ldbm42:tituxboy69971210203AS_@localhost:3306/id19445098_modernkitchendb")
+CLEARDB_DATABASE_URL = "mysql+pymysql://bd213d79e5d0f0:e9dda132@us-cdbr-east-06.cleardb.net/heroku_cc73e3a97a02270?reconnect=true"
+engine = create_engine(CLEARDB_DATABASE_URL)
 # meta = MetaData()
 
 conn = engine.connect()
